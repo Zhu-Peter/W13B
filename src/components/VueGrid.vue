703c9afa65b96eb8d221712b42b1ea61c7d1f1e7
Zhu-Peter <gzhu@ualberta.ca>
2024-05-03 19:11:41 -0600

    initial commit

---
+++ src/components/VueGrid.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="VueGrid">
+
+    </div>
+</template>
+
+  
+
+<script>
+
+export default {
+
+ name: 'VueGrid',
+
+ props: {
+ msg: String
+
+ }
+
+}
+
+</script>
+
+  
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped></style>
